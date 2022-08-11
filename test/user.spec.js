8a0013065740c58590cfd34843a8c6f9b19ada7c
김대석 <skywind4225@gmail.com>
2022-08-11 19:30:50 +0900

    user, login 추가 수정

--- test/user.spec.js
+++ test/user.spec.js
@@ -1,10 +1,10 @@
-const users = require("../routes/users");
+const app = require("../app");
 const {createUser} = require("../controllers/users.controller");
 const authMiddleware = require("../middlewares/auth-middleware");
 const request = require("supertest");
 
 test("비밀번호와 닉네임이 같은 값이 포함되어 회원가입에 실패하였습니다", async () => {
-    await request(users).post("/users").expect(createUser({nickname: "Test", password: "A1234", confirm: "A1234"})).toEqual(true);
+    request(app).post("/api/users").expect(createUser({nickname: "Test", password: "A1234", confirm: "A1234"})).toEqual(true);
     // expect(createUser({nickname: "Test", password: "A1234", confirm: "A1234"})).toEqual(true);
     // expect(createUser({nickname: "Test", password: "Test123", confirm: "Test123"})).toEqual(true);
 });
